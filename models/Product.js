c36fa4517458c3beca27b06958f4d0db6e746e13
Temporary User <temp@example.com>
2025-05-21 20:57:13 +0300

    Add models

---
+++ models/Product.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    taxes: {
+        type: Number,
+        default: 0
+    },
+    ads: {
+        type: Number,
+        default: 0
+    },
+    discount: {
+        type: Number,
+        default: 0
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema); 
